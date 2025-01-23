b329cb174915e966bd23d78d93f77c73014a9254
Gabow2002 <chessgabo@hotmail.com>
2025-01-23 04:18:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const calendar = document.getElementById("calendar-view");
     const startDate = new Date(2025, 0, 1); // Enero 1, 2025
+
     for (let i = 0; i < 31; i++) {
         let currentDate = new Date(startDate);
         currentDate.setDate(startDate.getDate() + i);
-        let formattedDate = currentDate.toISOString().split('T')[0];
+        let formattedDate = currentDate.toISOString().split('T')[0];  // Formato YYYY-MM-DD
 
         let dayElement = document.createElement("div");
         dayElement.className = "day";
-        dayElement.innerText = `${currentDate.toLocaleDateString('es-ES', { weekday: 'long', day: 'numeric' })}`;
+        dayElement.innerText = formattedDate;
         dayElement.onclick = () => window.location.href = `day.html?day=${formattedDate}`;
         calendar.appendChild(dayElement);
     }
